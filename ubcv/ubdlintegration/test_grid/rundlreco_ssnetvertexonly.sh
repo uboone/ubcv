b4e90f71cb7f4e73334cd062742db3dd2325e318
Samantha Sword-Fehlberg <ssfehlberg@gmail.com>
2019-11-22 13:59:14 -0600

    adding the good xml file and updating the tests_grid scripts to match the ones inside of ubdlintegration

--- ubcv/ubdlintegration/test_grid/rundlreco_ssnetvertexonly.sh
+++ ubcv/ubdlintegration/test_grid/rundlreco_ssnetvertexonly.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "<<<<<< RUN DL RECO SCRIPT >>>>>>"
-
 # OUTPUT FILES FROM PREVIOUS STAGE
 source /cvmfs/uboone.opensciencegrid.org/products/setup_uboone.sh
 
@@ -20,7 +19,23 @@ RECO2D=larlite_reco2d.root
 BACKTRACKER=larlite_backtracker.root
 
 echo "<<< CHECKING TO SEE IF THE FILE IS EMPTY >>>"
-python emptycheck.py $SUPERA
+py_script="
+import sys,os
+import larcv
+from larcv import larcv
+
+io = larcv.IOManager(larcv.IOManager.kREAD)
+io.add_in_file('%s'%sys.argv[1])
+io.initialize()
+
+nentries = io.get_n_entries()
+
+if nentries==0: 
+     sys.exit(1)
+else:
+     sys.exit(0)
+"
+python -c "$py_script" $SUPERA
 ret=$?
 echo $?
 
@@ -54,7 +69,7 @@ run_tagger $TAGGER_CONFIG
 
 TAGGER_LARCV=tagger_anaout_larcv.root
 TAGGER_LARLITE=tagger_anaout_larlite.root
-
+n
 echo "<<< RUN VERTEXER >>>"
 VERTEX_CONFIG=$DLLEE_UNIFIED_DIR/dlreco_scripts/vertex_configs/prod_fullchain_mcc9ssnet_combined_newtag_extbnb_c10_union.cfg
 #VERTEX_CONFIG=prod_fullchain_mcc9ssnet_combined_newtag_extbnb_c10_union.cfg # for debug
@@ -69,6 +84,24 @@ TRACKEROUT=tracker_reco.root
 TRACKERANA=tracker_anaout.root
 mv -f tracker_reco_0.root $TRACKEROUT
 mv -f tracker_anaout_0.root  $TRACKERANA
+
+#echo "<<< TRACKONLY CHAIN >>>"
+#echo "< RUN TRACKONLY VERTEXER >"
+#TRKONLY_VERTEX_CONFIG=$DLLEE_UNIFIED_DIR/dlreco_scripts/vertex_configs/prod_fullchain_alltracklabel_combined_newtag_extbnb_c10_union.cfg
+##TRKONLY_VERTEX_CONFIG=prod_fullchain_alltracklabel_combined_newtag_extbnb_c10_union.cfg # for debug
+#python $DLLEE_UNIFIED_DIR/dlreco_scripts/bin/run_vertexer.py -c $TRKONLY_VERTEX_CONFIG -a vertexana_trackonly_temp.root -o vertexout_trackonly.root -d ./ $SUPERA $TAGGER_LARCV
+#TRKONLY_VERTEXOUT=vertexout_trackonly.root
+#TRKONLY_VERTEXANA=vertexana_trackonly.root
+#python $DLLEE_UNIFIED_DIR/dlreco_scripts/bin/rename_vertexana.py vertexana_trackonly_temp.root $TRKONLY_VERTEXANA
+
+#echo "< RUN TRACKONLY TRACKER >"
+#TRKONLY_TRACKER_CONFIG=$DLLEE_UNIFIED_DIR/dlreco_scripts/tracker_configs/tracker_read_cosmo_trackonlyvertexer.cfg
+##TRKONLY_TRACKER_CONFIG=tracker_read_cosmo_trackonlyvertexer.cfg
+#python $DLLEE_UNIFIED_DIR/dlreco_scripts/bin/run_tracker_reco3d.py -c $TRKONLY_TRACKER_CONFIG -i $SUPERA -t $TAGGER_LARCV -p $TRKONLY_VERTEXOUT -d ./ 
+#TRKONLY_TRACKEROUT=tracker_reco_trackonly.root
+#TRKONLY_TRACKERANA=tracker_anaout_trackonly.root
+#mv -f tracker_reco_0.root    $TRKONLY_TRACKEROUT
+#mv -f tracker_anaout_0.root  $TRKONLY_TRACKERANA
 
 echo "<<< RUN SHOWER RECO >>>"
 echo "  < make inter file > "
@@ -92,8 +125,8 @@ hadd -f merged_dlreco.root $VERTEXOUT $VERTEXANA $TRACKERANA $SUPERA nueid_lcv_o
 #echo "<<< Append UBDL Products >>>"
 #python $DLLEE_UNIFIED_DIR/dlreco_scripts/bin/append_ubdlproducts.py merged_dlreco.root supera.root
 
-echo "<<< cleanup excess root files >>>"
-rm -f larlite_dlmerged.root larlite_larflow.root larlite_opreco.root larlite_reco2d.root larlite_mcinfo.root larlite_backtracker.root supera.root out_larcv_test.root larcv.root
-rm -f tagger_anaout_larcv.root tagger_anaout_larlite.root tracker_anaout.root tracker_reco.root vertexana.root vertexout.root
-rm -f shower_reco_out_0.root nueid_lcv_out_0.root nueid_ll_out_0.root lcv_trash.root nueid_ana_0.root
+#echo "<<< cleanup excess root files >>>"
+#rm -f larlite_dlmerged.root larlite_larflow.root larlite_opreco.root larlite_reco2d.root larlite_mcinfo.root larlite_backtracker.root supera.root out_larcv_test.root larcv.root
+#rm -f tagger_anaout_larcv.root tagger_anaout_larlite.root tracker_anaout.root tracker_reco.root vertexana.root vertexout.root
+#rm -f shower_reco_out_0.root nueid_lcv_out_0.root nueid_ll_out_0.root lcv_trash.root nueid_ana_0.root
 
--- ubcv/ubdlintegration/test_grid/rundlreco_ssnetvertexonly_ext.sh
+++ ubcv/ubdlintegration/test_grid/rundlreco_ssnetvertexonly_ext.sh
@@ -12,7 +12,23 @@ OPRECO=larlite_opreco.root
 RECO2D=larlite_reco2d.root
 
 echo "<<< CHECKING TO SEE IF THE FILE IS EMPTY >>>"
-python emptycheck.py $SUPERA
+py_script="
+import sys,os
+import larcv
+from larcv import larcv
+
+io = larcv.IOManager(larcv.IOManager.kREAD)
+io.add_in_file('%s'%sys.argv[1])
+io.initialize()
+
+nentries = io.get_n_entries()
+
+if nentries==0: 
+     sys.exit(1)
+else:
+     sys.exit(0)
+"
+python -c "$py_script" $SUPERA
 ret=$?
 echo $?
 
@@ -62,6 +78,24 @@ TRACKERANA=tracker_anaout.root
 mv -f tracker_reco_0.root $TRACKEROUT
 mv -f tracker_anaout_0.root  $TRACKERANA
 
+#echo "<<< TRACKONLY CHAIN >>>"
+#echo "< RUN TRACKONLY VERTEXER >"
+#TRKONLY_VERTEX_CONFIG=$DLLEE_UNIFIED_DIR/dlreco_scripts/vertex_configs/prod_fullchain_alltracklabel_combined_newtag_extbnb_c10_union.cfg
+##TRKONLY_VERTEX_CONFIG=prod_fullchain_alltracklabel_combined_newtag_extbnb_c10_union.cfg # for debug
+#python $DLLEE_UNIFIED_DIR/dlreco_scripts/bin/run_vertexer.py -c $TRKONLY_VERTEX_CONFIG -a vertexana_trackonly_temp.root -o vertexout_trackonly.root -d ./ $SUPERA $TAGGER_LARCV
+#TRKONLY_VERTEXOUT=vertexout_trackonly.root
+#TRKONLY_VERTEXANA=vertexana_trackonly.root
+#python $DLLEE_UNIFIED_DIR/dlreco_scripts/bin/rename_vertexana.py vertexana_trackonly_temp.root $TRKONLY_VERTEXANA
+
+#echo "< RUN TRACKONLY TRACKER >"
+#TRKONLY_TRACKER_CONFIG=$DLLEE_UNIFIED_DIR/dlreco_scripts/tracker_configs/tracker_read_cosmo_trackonlyvertexer.cfg
+##TRKONLY_TRACKER_CONFIG=tracker_read_cosmo_trackonlyvertexer.cfg
+#python $DLLEE_UNIFIED_DIR/dlreco_scripts/bin/run_tracker_reco3d.py -c $TRKONLY_TRACKER_CONFIG -i $SUPERA -t $TAGGER_LARCV -p $TRKONLY_VERTEXOUT -d ./ 
+#TRKONLY_TRACKEROUT=tracker_reco_trackonly.root
+#TRKONLY_TRACKERANA=tracker_anaout_trackonly.root
+#mv -f tracker_reco_0.root    $TRKONLY_TRACKEROUT
+#mv -f tracker_anaout_0.root  $TRKONLY_TRACKERANA
+
 echo "<<< RUN SHOWER RECO >>>"
 echo "  < make inter file > "
 NUEID_INTER_DIR=${LARLITECV_BASEDIR}/app/LLCVProcessor/InterTool/Sel/NueID/mac/ # using ups
@@ -81,8 +115,8 @@ echo "<< combine larlite files >>"
 python $DLLEE_UNIFIED_DIR/dlreco_scripts/bin/combine_larlite.py -o larlite_dlmerged.root larlite_opreco.root larlite_reco2d.root tagger_anaout_larlite.root tracker_reco.root nueid_ll_out_0.root shower_reco_out_0.root
 echo "<<< HADD ROOT FILES >>>"
 hadd -f merged_dlreco.root $VERTEXOUT $VERTEXANA $TRACKERANA nueid_lcv_out_0.root larlite_dlmerged.root
-echo "<<< Append UBDL Products >>>"
-python $DLLEE_UNIFIED_DIR/dlreco_scripts/bin/append_ubdlproducts.py merged_dlreco.root out_larcv_test.root
+#echo "<<< Append UBDL Products >>>"
+#python $DLLEE_UNIFIED_DIR/dlreco_scripts/bin/append_ubdlproducts.py merged_dlreco.root out_larcv_test.root
 
 #echo "<<< cleanup excess root files >>>"
 #rm -f larlite_dlmerged.root larlite_larflow.root larlite_opreco.root larlite_reco2d.root out_larcv_test.root 
