b64c90e8cfa7e109d3f0201a85a0134f7ab5eccf
Matthew Rosenberg <rosenberg6m@gmail.com>
2025-04-25 10:29:35 -0400

    updated lantern launch scripts with fix needed to run on the grid

--- ubcv/LArCVImageMaker/mcc10_prod_scripts/launch_lantern_container_mc_unified.sh
+++ ubcv/LArCVImageMaker/mcc10_prod_scripts/launch_lantern_container_mc_unified.sh
@@ -2,8 +2,7 @@
 
 echo "executing run script from cvmfs lantern container"
 
-SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
-/cvmfs/oasis.opensciencegrid.org/mis/apptainer/1.3.2/bin/apptainer exec --cleanenv /cvmfs/uboone.opensciencegrid.org/containers/lantern_v2_me_06_03_prod/ "$SCRIPT_DIR/run_lantern_workflow_mc.sh"
+/cvmfs/oasis.opensciencegrid.org/mis/apptainer/1.3.2/bin/apptainer exec --cleanenv /cvmfs/uboone.opensciencegrid.org/containers/lantern_v2_me_06_03_prod/ /cluster/home/lantern_scripts/run_lantern_workflow_mc.sh
 
 echo "Container exited with code $?"
 
